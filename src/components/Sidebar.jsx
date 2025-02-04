4e212957cce1a59b327b6ca54131f2782c3887cd
br907888 <br907888@ucf.edu>
2025-02-04 18:29:42 -0500

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,15 @@
 import { useState, useCallback } from "react"
-import React from 'react';
 /**
  * Renders an array of strings passed in that can be filtered and added to as an
  * unordered list.
  * @returns Component
  */
-export default function Sidebar() {
+export default function Sidebar({ initialMenuItems = [] }) {
   let [newMenuItem, setNewMenuItem] = useState("")
   // TODO: 2 Using a state hook, maintain the current menu items as an array state.
   // let [menuItems, setMenuItems] = useState(initialMenuItems)
-  let [filter, setFilter] = useState("")
-  import React, {useState} from 'react';
-  function Menu({initialMenuItems}) {
-    const[menuItems, setMenuItems] = useState(initialMenuItems);
-    return(
-      );
-  }
+  let [menuItems, setMenuItems] = useState(initialMenuItems);
+  let [filter, setFilter] = useState("");
   // Adds a single string passed in as parameter to the state element
   // "menuItems" that holds the set of current menu items.
   let addMenuItem = useCallback(() => {
@@ -23,73 +17,27 @@ export default function Sidebar() {
     //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
     //   // This involves adding a parameter and changing a class instance variable (props).
     //   setMenuItems([item, ...menuItems])
-  }, [])
-  function MenuNew({children}){
-    const [menuItems, setItems] = useState([]);
-    const addMenuItem = (item) => {
-      setMenuItems([item, ...menuItems]);
-    };
-    return (
-      <div>
-        {}
-      <button onClick=() => admenuItem({ name: 'New Item'})}>Add Item</div>button>
-      <ul>
-        {menuItems.map((item, index)=> (
-        <li key=(index)>(item.name)</li>
-        ))}
-        </ul>
-      </div>
-      );
-  }
-  export default MenuNew;
+    if (newMenuItem.trim() !== "") {
+      setMenuItems((primeMenuItems) => [...primeMenuItems, newMenuItem]);
+      setNewMenuItem("");
+    }
+  }, [newMenuItem])
+
   // TODO: 4. Display ONLY the menu items that contain the filter element value
   // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
-function FilterableMenu({ menuItems }) {
-  const [filterText, setFilterText] = useState('');
-
-  const handleFilterChange = (event) => {
-    setFilterText(event.target.value);
-  };
-
-  const filteredItems = menuItems.filter((item) => {
-    const regex = new RegExp(filterText, 'i'); 
-    return regex.test(item.label); 
-  });
-
-  return (
-    <div>
-      <input 
-        type="text" 
-        placeholder="Search menu items" 
-        value={filterText} 
-        onChange={handleFilterChange} 
-      />
-      <ul>
-        {filteredItems.map((item) => (
-          <li key={item.id}>{item.label}</li>
-        ))}
-      </ul>
-    </div>
+  const menuItemFilter = menuItems.filter((item) =>
+    item.toLowerCase().includes(filter.toLocaleLowerCase())
   );
-}
 
-export default FilterableMenu;
   // TODO: 1 Render inside the outer div an unordered list of the menu items, with each string in the array
   // its own item.
-  import React from 'react';
-  function MenuList({ initialMenuItems }) {
-    return (
-      <ul>
-        {intialMenuItems.map((item, index =>(
-        <li key={index}>{item}</li>
-        ))}
-      </ul>
-      );
-  }
-  
-  export default MenuList;
   return (
     <div>
+      <ul>
+        {menuItemFilter.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
       <input
         type="text"
         id="newMenuItemValue"
@@ -98,9 +46,9 @@ export default FilterableMenu;
       ></input>
       <br />
       <button
-        onClick={() => {
-          /* TODO: 3 */
-        }}
+        onClick={addMenuItem}
+      /* TODO: 3 */
+
       >
         Add Item
       </button>
@@ -112,6 +60,7 @@ export default FilterableMenu;
         onChange={(event) => setFilter(event.target.value)}
         placeholder="Filter by..."
       ></input>
+
     </div>
   )
 }
